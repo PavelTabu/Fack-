6802cb372bcae4e0a0b501754915aa1d627a51e6
Павел <pavel.tabu@gmail.com>
2015-10-24 14:49:30 +0400

    Kvest

--- src/File2.java
+++ src/File2.java
@@ -3,41 +3,9 @@ import java.io.FileReader;
 import java.io.IOException;
 public class File2 {
 
-    public static class BufferedReaderExample {
 
-        public static void main(String[] args) {
-
-            BufferedReader xz = null;
-
-            try {
-
-                String sCurrentLine;
-
-                xz = new BufferedReader(new FileReader("D:\\Rock.csv"));
-
-                while ((sCurrentLine = xz.readLine()) != null) {
-                    int i;
-                    String [] re = sCurrentLine.split(",");
-
-                      for (i=0;i<sCurrentLine.length();i++ )
-                    {
-
-                        System.out.println(re[i] + "  ");
-                    }
-
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (xz != null)xz.close();
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            }
+    public static void main(String[] args) {
 
 
-        }
-        }
+    }
 }
