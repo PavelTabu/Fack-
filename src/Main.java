366bde104a08bad52003d6a1c18a02c8d8865be9
kumar <amconsultllc@yahoo.com>
2015-10-24 14:40:46 +0300

    template for task

--- src/Main.java
+++ src/Main.java
@@ -6,18 +6,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("вот тут шо-нибудь введи (цифру):");
         Scanner sc = new Scanner(System.in);
-        switch (sc.nextInt()){ // смотрит, шо ты ввёл в консоль
-            case 1 : {
-                System.out.println("option 1");
-            }
-
-            case 2 : {
-                System.out.println("option 2");
-            }
-            default: {
-                System.out.printf("Operation is not supported");
-            }
-
-        }
+//        switch (sc.nextInt()){ // смотрит, шо ты ввёл в консоль
+//            case 1 : {
+//                System.out.println("option 1");
+//            }
+//            case 2 : {
+//                System.out.println("option 2");
+//            }
+//            default: {
+//                System.out.printf("Operation is not supported");
+//            }
+//
+//        }
     }
 }
